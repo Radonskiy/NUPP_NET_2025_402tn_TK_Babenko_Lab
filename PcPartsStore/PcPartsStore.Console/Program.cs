a3bf592e8dd0b11f5d01a422f969570f1141a4a2
Олександр Бабенко <babenko.sanya248@gmail.com>
2025-12-21 20:29:29 +0200

    v2

--- PcPartsStore/PcPartsStore.Console/Program.cs
+++ PcPartsStore/PcPartsStore.Console/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using PcPartsStore.Common.Extensions;
 using PcPartsStore.Common.Models;
 using PcPartsStore.Common.Services;
 
@@ -14,29 +14,33 @@ namespace PcPartsStore.Console
             var gpu = new Gpu("RTX 5070", 32000m, 12, "NVIDIA", 250);
             var cpu = new Cpu("Ryzen 7 9800X3D", 22000m, 8, 16, 4.7m);
 
+            // CREATE
             crud.Create(gpu);
             crud.Create(cpu);
 
             global::System.Console.WriteLine("=== ПІСЛЯ CREATE ===");
             foreach (var p in crud.ReadAll())
-                global::System.Console.WriteLine(p.GetInfo());
+                global::System.Console.WriteLine(p.ToShortString()); // extension method
 
+            // READ
             global::System.Console.WriteLine("\n=== READ (GPU) ===");
             var found = crud.Read(gpu.Id);
-            global::System.Console.WriteLine(found != null ? found.GetInfo() : "Не знайдено");
+            global::System.Console.WriteLine(found != null ? found.ToShortString() : "Не знайдено");
 
+            // UPDATE
             global::System.Console.WriteLine("\n=== UPDATE (GPU price) ===");
             gpu.Price = 29999m;
             crud.Update(gpu);
 
             foreach (var p in crud.ReadAll())
-                global::System.Console.WriteLine(p.GetInfo());
+                global::System.Console.WriteLine(p.ToShortString());
 
+            // REMOVE
             global::System.Console.WriteLine("\n=== REMOVE (CPU) ===");
             crud.Remove(cpu.Id);
 
             foreach (var p in crud.ReadAll())
-                global::System.Console.WriteLine(p.GetInfo());
+                global::System.Console.WriteLine(p.ToShortString());
 
             global::System.Console.WriteLine("\nГотово. Enter...");
             global::System.Console.ReadLine();
